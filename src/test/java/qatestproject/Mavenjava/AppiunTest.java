abcebb37851ee31ab797a16cda1937fa57342f65
23ravi89 <ravirahul1988@gmail.com>
2021-05-10 10:13:52 +0530

    first commit

---
+++ src/test/java/qatestproject/Mavenjava/AppiunTest.java
@@ -0,0 +1,20 @@
+package qatestproject.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiunTest {
+	
+	
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("NativeAppAndroid");
+	}
+	
+	@Test
+	public void IosApp()
+	{
+		System.out.println("IosApp");
+	}
+
+}
---
+++ src/test/java/qatestproject/Mavenjava/Se4leniumTest.java
@@ -0,0 +1,22 @@
+package qatestproject.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class Se4leniumTest {
+	
+	//testng xml file fom maven
+	//how to execute all test cases from test folder -mvn test
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementui()
+	{
+		System.out.println("elementui");
+	}
+	
+
+}
